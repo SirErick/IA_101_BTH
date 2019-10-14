1abe19e3d0316ff4cd1812a3f441c7550e137d62
Erick M. Sirpa <erickhobo98@gmail.com>
2019-10-14 17:51:03 -0400

    Create localization.py

---
+++ localization.py
@@ -0,0 +1,165 @@
+p = [0.2,0.2,0.2,0.2,0.2]
+
+print p
+
+//-----------
+
+p=[]
+n=5
+for i in range (n):
+    p.append(1./n)
+
+print p
+
+//-----------
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+pHit = 0.6
+pMiss = 0.2
+
+p[0]=p[0]*pMiss
+p[1]=p[1]*pHit
+p[2]=p[2]*pHit
+p[3]=p[3]*pMiss
+p[4]=p[4]*pMiss
+
+print p
+
+//-------------
+
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+pHit = 0.6
+pMiss = 0.2
+
+p[0]=p[0]*pMiss
+p[1]=p[1]*pHit
+p[2]=p[2]*pHit
+p[3]=p[3]*pMiss
+p[4]=p[4]*pMiss
+
+sum= p[1]+p[2]+p[3]+p[4]+p[0]
+print sum
+
+//-----------------
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+Z = 'red'
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    #
+    #ADD YOUR CODE HERE
+    #
+    return q
+
+print sense(p,Z)
+
+//----------------------
+
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+Z = 'red'
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(p)):
+        q[i]=q[i]/s 
+    return q
+print sense(p, Z)
+
+
+//-------------------
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+measurements = ['red', 'green']
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
+#
+#ADD YOUR CODE HERE
+#
+print p
+
+//----------------
+Multiples medidas
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+world=['green', 'red', 'red', 'green', 'green']
+measurements = ['red', 'green']
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
+#
+#ADD YOUR CODE HERE
+#
+print sense(p,measurements)
+
+//-----------
+
+Funcion Mover
+p=[0, 1, 0, 0, 0]
+world=['green', 'red', 'red', 'green', 'green']
+measurements = ['red', 'green']
+pHit = 0.6
+pMiss = 0.2
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i])
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
+
+
+    
+def move(p, U):
+    q = []
+    for i in range(len(p)):
+        print i
+        q.append(p[(i-U)%len(p)])
+    return q
+    
+
+print move(p, 1)
+
+//-------------
+
+
+
+
+
+
+
+
